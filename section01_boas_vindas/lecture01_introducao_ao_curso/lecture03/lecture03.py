d9b66aad8a71a358844b2d438733f48149567a67
calebeLAR <ca6ele@gmail.com>
2024-01-25 20:41:37 -0300

    trybe_ia_aws.section01.lecture03.py

---
+++ section01_boas_vindas/lecture01_introducao_ao_curso/lecture03/lecture03.py
@@ -0,0 +1,8 @@
+class Unit03:
+    title = "Conhecendo nossa comunidade"
+    topics = [
+        "A Trybe está no Whatsapp!",
+        "Normas da comunidade",
+        "O que valorizamos por lá",
+        "O que não permitimos por lá",
+    ]
