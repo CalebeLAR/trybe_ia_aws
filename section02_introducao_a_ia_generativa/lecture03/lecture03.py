41d4043a5dc23327afdc8435b2473c639897628b
calebeLAR <ca6ele@gmail.com>
2024-01-26 16:28:34 -0300

    trybe.fundamentos.secao07.section02.lecture03c.py

---
+++ section02_introducao_a_ia_generativa/lecture03/lecture03.py
@@ -0,0 +1,8 @@
+class Lecture03:
+    title = "Como as máquinas aprendem"
+    topics = [
+        "Aprendizado de Máquina",
+        "Habilidade das máquinas de aprender",
+        "Métodos de Aprendizado de Máquina",
+        "Aplicações do Aprendizado de Máquina",
+    ]
