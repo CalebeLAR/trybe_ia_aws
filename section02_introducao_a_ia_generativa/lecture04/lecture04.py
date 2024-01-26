779fb1f15719964da14be72d11abedeee7599f3a
calebeLAR <ca6ele@gmail.com>
2024-01-26 16:28:34 -0300

    trybe.fundamentos.secao07.section02.lecture04c.py

---
+++ section02_introducao_a_ia_generativa/lecture04/lecture04.py
@@ -0,0 +1,7 @@
+class Lecture04:
+    title = "O que é IA generativa"
+    topics = [
+        "O que é IA Generativa",
+        "Fundamentos e Características da IA Generativa",
+        "Aplicações Práticas da IA Generativa",
+    ]
