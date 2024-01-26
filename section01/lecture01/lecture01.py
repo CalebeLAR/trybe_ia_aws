e5ad12b01a47aebaa574057d40c2607ac9c1f7de
calebeLAR <ca6ele@gmail.com>
2024-01-26 16:29:45 -0300

    trybe.fundamentos.secao07.section01.lecture01c.py

---
+++ section01/lecture01/lecture01.py
@@ -0,0 +1,8 @@
+class Unit01:
+    title = "Introdução ao curso"
+    topics = [
+        "Boas vindas ao curso",
+        "Como será sua Jornada?",
+        "O que esperar ao final deste curso?",
+        "E no mercado de trabalho?",
+    ]
