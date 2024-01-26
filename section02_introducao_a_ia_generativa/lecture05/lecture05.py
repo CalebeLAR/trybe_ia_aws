4a45c92612363ff7ae5a3b373d24128009485295
calebeLAR <ca6ele@gmail.com>
2024-01-26 16:28:34 -0300

    trybe.fundamentos.secao07.section02.lecture05c.py

---
+++ section02_introducao_a_ia_generativa/lecture05/lecture05.py
@@ -0,0 +1,11 @@
+class Lecture05:
+    title = "Conceitos úteis"
+    topics = [
+        "Conceitos Úteis em IA",
+        "APIs de IA",
+        "Engenharia de Prompting",
+        "Conceito de Prompts",
+        "Console e Terminal",
+        "Modelagem e Avaliação em IA",
+        "Ética e Responsabilidade no Uso da IA",
+    ]
