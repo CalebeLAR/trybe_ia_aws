b445c954f99f6d03b02245e25e9db2250a402296
calebeLAR <ca6ele@gmail.com>
2024-01-27 17:49:57 -0300

    trybe_ia_aws.section03.lecture07.py

---
+++ section03_conhecendo_o_codewhisperer/lecture07/lecture07.py
@@ -0,0 +1,7 @@
+class Lecture07:
+    title = "Referências e recursos adicionais"
+    topics = [
+        "Referências e recursos adicionais",
+        "Referências",
+        "Recursos adicionais",
+    ]
