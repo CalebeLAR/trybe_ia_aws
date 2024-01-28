a93a83ab3ef7a45384d9e12ee8ac0f500dfbfb84
calebeLAR <ca6ele@gmail.com>
2024-01-28 19:37:34 -0300

    trybe_ia_aws.section04.lecture02.py

---
+++ section04_introducao_ao_amazon_bedrock/lecture02/lecture02.py
@@ -0,0 +1,8 @@
+class Lecture02:
+    title = "Para que serve o Bedrock"
+    topics = [
+        "Apresentação do problema que podemos resolver",
+        "Complexidade de uma simples lei",
+        "Solução - O Amazon Bedrock",
+        "Amazon Bedrock e seu poder",
+    ]
