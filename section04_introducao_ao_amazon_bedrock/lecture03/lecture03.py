804adea0ce6e016c22f193b1bb7872520e138cf1
calebeLAR <ca6ele@gmail.com>
2024-01-28 19:37:34 -0300

    trybe_ia_aws.section04.lecture03.py

---
+++ section04_introducao_ao_amazon_bedrock/lecture03/lecture03.py
@@ -0,0 +1,9 @@
+class Lecture03:
+    title = "Configuração de acesso ao Amazon Bedrock"
+    topics = [
+        "Configuração de acesso ao Amazon Bedrock",
+        "Abrir o Bedrock no AWS Console",
+        "Acesso aos modelos",
+        "Playground",
+        "Criação de usuário e permissão de uso do Bedrock",
+    ]
