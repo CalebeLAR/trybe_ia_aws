a8ee7e55f88aa32838852df730690451d288d76a
calebeLAR <ca6ele@gmail.com>
2024-01-29 17:03:41 -0300

    trybe_ia_aws.section04.lecture05.py

---
+++ section04_introducao_ao_amazon_bedrock/lecture05/lecture05.py
@@ -0,0 +1,17 @@
+class Lecture05:
+    title = "Conhecendo a ferramenta"
+    topics = [
+        "Apresentação da Ferramenta",
+        "Praticar - Amazon Bedrock Workshop",
+        "Código do vídeo",
+    ]
+
+
+# fine turner
+# 13:54 - tokes: 10 tokens ~ 7 palvra
+# 14:00 - cloudWatch: monitorar preços
+# o acessso à IDE é gratuito, mas o kernel é pago
+# amazom sageMaker: domain: IDE jupter aws
+
+# Amazon Bedrock Workshop
+# https://catalog.us-east-1.prod.workshops.aws/workshops/a4bdb007-5600-4368-81c5-ff5b4154f518/en-US
