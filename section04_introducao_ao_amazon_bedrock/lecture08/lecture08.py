738cafb88e5f53d480bfa90cc2dae6d6a20d051e
calebeLAR <ca6ele@gmail.com>
2024-01-29 17:03:41 -0300

    trybe_ia_aws.section04.lecture08.py

---
+++ section04_introducao_ao_amazon_bedrock/lecture08/lecture08.py
@@ -0,0 +1,14 @@
+class Lecture08:
+    title = "Referências e recursos adicionais"
+    topics = [
+        "Referências e recursos adicionais",
+        "Referências",
+        "Recursos adicionais",
+    ]
+
+
+# Recursos adicionais
+# AWS Identity and Access Management
+# https://aws.amazon.com/pt/iam/. Acesso em: 01/12/2023
+# Nível gratuito da AWS - Site da AWS.
+# https://aws.amazon.com/pt/free/. Acesso em: 01/12/2023
