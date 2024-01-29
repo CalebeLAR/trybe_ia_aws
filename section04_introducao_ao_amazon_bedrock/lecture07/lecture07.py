82dddef9af6c9632c7e5457dc323f913de19f0f5
calebeLAR <ca6ele@gmail.com>
2024-01-29 17:03:41 -0300

    trybe_ia_aws.section04.lecture07.py

---
+++ section04_introducao_ao_amazon_bedrock/lecture07/lecture07.py
@@ -0,0 +1,9 @@
+class Lecture07:
+    title = "Para não esquecer mais"
+    topics = [
+        "Para não esquecer mais",
+        "Para onde vamos?",
+    ]
+
+# casos de sucesso
+# https://aiexplorer.aws.amazon.com/success-stories?lang=pt
