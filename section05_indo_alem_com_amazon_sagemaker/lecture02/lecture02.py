7a765f8a23c3731a9ffee80a6cd0de6cd895c17e
calebeLAR <ca6ele@gmail.com>
2024-01-30 16:50:23 -0300

    trybe_ia_aws.section05.lecture02.py

---
+++ section05_indo_alem_com_amazon_sagemaker/lecture02/lecture02.py
@@ -0,0 +1,6 @@
+class Lecture02:
+    title = "Apresentando o problema que iremos resolver"
+    topics = [
+        "Caso de uso: assistente personalizado alimentado por dados super específicos",
+        "Que ferramenta usar?",
+    ]
