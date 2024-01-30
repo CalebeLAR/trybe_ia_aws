df446e5ab93ebe94ae4b7da4c988dc5594dc303e
calebeLAR <ca6ele@gmail.com>
2024-01-30 16:50:23 -0300

    trybe_ia_aws.section05.lecture03.py

---
+++ section05_indo_alem_com_amazon_sagemaker/lecture03/lecture03.py
@@ -0,0 +1,10 @@
+class Lecture03:
+    title = "Como acessar o Sagemaker"
+    topics = [
+        "Como acessar o Sagemaker?",
+        "Quando a cobrança vem?",
+    ]
+
+
+# Amazon Sagemaker Workshop
+# https://catalog.us-east-1.prod.workshops.aws/workshops/0c6b8a23-b837-4e0f-b2e2-4a3ffd7d645b/en-US/getting-started
