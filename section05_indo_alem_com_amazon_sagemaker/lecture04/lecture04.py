42ec12940ac607b78fcd14a184b863a2a2b4bf23
calebeLAR <ca6ele@gmail.com>
2024-01-30 16:50:23 -0300

    trybe_ia_aws.section05.lecture04.py

---
+++ section05_indo_alem_com_amazon_sagemaker/lecture04/lecture04.py
@@ -0,0 +1,8 @@
+class Lecture04:
+    title = "Conhecendo a ferramenta"
+    topics = [
+        "Conhecendo o Sagemaker",
+        "Para fixar",
+        "Exercício 1 - Acesse o Sagemaker",
+        "Exercício 2 - Amazon Sagemaker Workshop",
+    ]
