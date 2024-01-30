2d955a65a40fff11baa3f510f530b69b64f747a6
calebeLAR <ca6ele@gmail.com>
2024-01-30 16:50:23 -0300

    trybe_ia_aws.section05.lecture07.py

---
+++ section05_indo_alem_com_amazon_sagemaker/lecture07/lecture07.py
@@ -0,0 +1,18 @@
+class Lecture06:
+    title = "Referências e recursos adicionais"
+    topics = [
+        "Referências e recursos adicionais",
+        "Referências",
+        "Recursos adicionais",
+    ]
+
+# Sagemaker Studio Jumpstart - Docs
+# https://docs.aws.amazon.com/sagemaker/latest/dg/studio-jumpstart.html
+# Sagemaker 101 Workshop
+# https://catalog.us-east-1.prod.workshops.aws/workshops/0c6b8a23-b837-4e0f-b2e2-4a3ffd7d645b/en-US/getting-started
+# Hugging Face
+# https://huggingface.co/
+# Github - Sagemaker 101 Workshop Samples
+# https://github.com/aws-samples/sagemaker-101-workshop
+# AWS Cost Explorer
+# https://docs.aws.amazon.com/sagemaker/latest/dg/algos.html
