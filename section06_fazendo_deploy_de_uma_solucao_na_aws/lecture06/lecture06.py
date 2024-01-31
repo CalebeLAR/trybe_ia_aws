e18677a26d3c8b6b64b77962f43ec9f3416c823a
calebeLAR <ca6ele@gmail.com>
2024-01-31 18:29:46 -0300

    trybe_ia_aws.section06.lecture06.py

---
+++ section06_fazendo_deploy_de_uma_solucao_na_aws/lecture06/lecture06.py
@@ -0,0 +1,26 @@
+class Lecture06:
+    title = "Referências e recursos adicionais"
+    topics = [
+        "Referências e recursos adicionais",
+        "Referências",
+        "Recursos adicionais",
+    ]
+
+
+# AWS - Documentação sobre precificação
+# https://aws.amazon.com/pt/ec2/pricing/
+
+# AWS - Documentação sobre o Inferentia 2
+# https://aws.amazon.com/pt/ec2/instance-types/inf2/
+
+# AWS - Documentação sobre o Trainium
+# https://aws.amazon.com/pt/machine-learning/trainium/
+
+# AWS - Guia do usuário sobre o AWS Cost Explorer
+# https://docs.aws.amazon.com/cost-management/latest/userguide/ce-what-is.html?icmpid=docs_ach_docs_ach_help_panel
+
+# Inteligência Artificial responsável da AWS
+# https://aws.amazon.com/pt/machine-learning/responsible-ai/
+
+# Construtor de aplicações de IA generativa na AWS
+# https://docs.aws.amazon.com/pdfs/solutions/latest/generative-ai-application-builder-on-aws/generative-ai-application-builder-on-aws.pdf
